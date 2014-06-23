fd542c195a9075c26a2fc9905270850a5b8c5f56
Sean Gilligan <sean@msgilligan.com>
2014-06-22 22:58:25 -0700

    Smarter cleanup of forked process

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -x
 
+function cleanup {
+    kill $BTCPID
+}
+trap cleanup EXIT
+
 # Assume bitcoind built elsewhere and coied by Jenkins Copy Artifact plugin
 BTCD=copied-artifacts/src/bitcoind
 chmod +x $BTCD
@@ -10,10 +15,11 @@ mkdir -p regtest-datadir
 cp bitcoin.conf regtest-datadir
 mkdir logs
 $BTCD -server -regtest -datadir=regtest-datadir -debug > logs/bitcoin.log &
+BTCPID=$!
 
 # Run integration tests
 cd bitcoin-spock
 ./gradlew test
 
-kill $!
+exit $?
 
